db6cc0524ac8ef1a53b75d553d0f1e31a3a41008
Bnepster <103311338+Bnepster@users.noreply.github.com>
2022-10-12 19:37:24 -0300

    exercicio novo

---
+++ Curso_de_JS/Módulo_B/ex005.js
@@ -0,0 +1,47 @@
+
+        /*
+                Operadores aritiméticos
+        
+            + Adição            | / Divisão inteira
+            - Subtração         | % Resto da divisão
+           * multiplicação     | ** Potência         
+        
+
+         
+             Ordem de precedência
+
+            1 ()  Patêntese
+            2 **  Potẽncia
+            3 *  /  %  Multiplicação, Divisão inteira, Resto da divisão
+            4 +  -  Adição, Subtração
+        
+
+        
+            Operadores relacionais
+                > Maior     |  >= Maior ou igual
+                < Menor     |  <= Menor ou igual
+                == Igual    |  != Diferente
+
+                O operador de identidade é composto por três sinais de iggual ===
+        
+        
+            Operadores lógicos
+
+            ! Negação       operador NÃO
+            && Conjunção    operador E
+            || Disjunção    operador OU
+        
+
+        
+            Operadores ternários
+                Teste ? true : false
+        
+
+            Testando operadores ternários
+        */
+
+var n1 = 5
+var n2 = 3
+var m = (n1 + n2) / 2
+
+m >= 5 ? 'Média boa' : 'Média ruim'
