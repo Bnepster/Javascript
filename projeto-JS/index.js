16f5f3cd9324496edd083c53e9d092ce93e87897
Bruno de Sousa <103311338+Bnepster@users.noreply.github.com>
2023-03-21 21:28:21 -0300

    atualização

---
+++ projeto-JS/index.js
@@ -0,0 +1,9 @@
+function mostrar(){
+    let m = document.getElementById('msg')
+    m.innerText = 'TESTANDO...'
+}
+
+function most(){
+    let a = document.getElementById('ms2')
+    a.innerText = 'Novo teste'
+}
